9ee61cfd4f3f4ae12b4c683d21b634db65018294
phongho <phonght15591@gmail.com>
2022-12-22 22:54:18 +0700

    update

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,7 +1,10 @@
 #!/bin/bash  
+npm install --global yarn 
+nvm install node
 yarn install
 yarn build
  
+
 docker build -t kobiton/documentation:1.0 -f docker/documentation/Dockerfile .
 
 docker build -t kobiton/portal-help:1.0 -f docker/portal-help/Dockerfile .
