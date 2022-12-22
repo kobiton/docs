f962c77d5bded1a5055b3f806ba07faf9d04dd09
phongho <phonght15591@gmail.com>
2022-12-22 22:21:41 +0700

    update dockerize

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,9 +1,11 @@
 #!/bin/bash  
+cd ..
+yarn install
+yarn build
 
-cd ../docker/documentation
-pwd
+
+cd docker/documentation
 docker build -t kobiton/documentation:1.0 -f docker/documentation/Dockerfile .
 
-cd ../docker/portal-help
-pwd
+cd docker/portal-help
 docker build -t kobiton/portal-help:1.0 -f docker/portal-help/Dockerfile .
