46850e317f4f5d65ee15cc96a848fb123558c140
phongho <phonght15591@gmail.com>
2022-12-22 22:16:05 +0700

    update dockerize

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,6 +1,9 @@
 #!/bin/bash  
 
-cd ..
-
+cd ../docker/documentation
+pwd
 docker build -t kobiton/documentation:1.0 -f docker/documentation/Dockerfile .
+
+cd ../docker/portal-help
+pwd
 docker build -t kobiton/portal-help:1.0 -f docker/portal-help/Dockerfile .
