9fcf7cc969c00ca5a7f210ce908902818413d932
phongho@kobiton.com <100576666+phongho15@users.noreply.github.com>
2022-12-23 10:21:14 +0700

    Update dockerize.sh

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -14,7 +14,7 @@ docker_image_name2="${KOBITON_CI_DOCKER_IMAGE_NAME:-portal-help}"
 docker_image_tag2="${KOBITON_CI_DOCKER_IMAGE_TAG:-latest}"
 
 cd ./docker/documentation
-docker build -t $docker_image_name1:$docker_image_tag1 --target app --build-arg COMMIT_ID=`git rev-parse --short HEAD` .
+docker build -t $docker_image_name1:$docker_image_tag1 --build-arg COMMIT_ID=`git rev-parse --short HEAD` .
 
 cd ./docker/portal-help 
-docker build -t $docker_image_name2:$docker_image_tag2 --target app --build-arg COMMIT_ID=`git rev-parse --short HEAD` .
+docker build -t $docker_image_name2:$docker_image_tag2 --build-arg COMMIT_ID=`git rev-parse --short HEAD` .
