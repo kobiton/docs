8503988a56352324cb7a28b0a59915e6270fe404
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-06-12 07:59:58 -0700

    Adding commit from #70

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -7,14 +7,14 @@ yarn install
 yarn build
  
 # These params are for CI to control the output docker image
-docker_image_name1="${KOBITON_CI_DOCKER_IMAGE_NAME:-documentation}"
+docker_image_name1="${KOBITON_CI_DOCKER_IMAGE_NAME:-docs}"
 docker_image_tag1="${KOBITON_CI_DOCKER_IMAGE_TAG:-latest}"
 
 docker_image_name2="${KOBITON_CI_DOCKER_IMAGE_NAME:-widget}"
 docker_image_tag2="${KOBITON_CI_DOCKER_IMAGE_TAG:-latest}"
 
 
-docker build -t documentation:$GIT_COMMIT_ID --build-arg COMMIT_ID=`git rev-parse --short HEAD` -f docker/documentation/Dockerfile .
+docker build -t docs:$GIT_COMMIT_ID --build-arg COMMIT_ID=`git rev-parse --short HEAD` -f docker/docs/Dockerfile .
 
 
 docker build -t widget:$GIT_COMMIT_ID --build-arg COMMIT_ID=`git rev-parse --short HEAD` -f docker/widget/Dockerfile .
