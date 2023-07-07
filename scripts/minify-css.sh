cc6d58bb3345159631c66dc4a6d7f8d1459842aa
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-07-07 11:01:24 -0700

    Updated minify script to not run if no changes to 'site.css' files

--- scripts/minify-css.sh
+++ scripts/minify-css.sh
@@ -4,27 +4,34 @@
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 # Set the temporary file path
-TEMP_FILE="$PROJECT_ROOT/ui-bundle-docs/css/temp.css"
+TEMP_FILE_DOCS="$PROJECT_ROOT/ui-bundle-docs/css/temp.css"
+TEMP_FILE_WIDGET="$PROJECT_ROOT/ui-bundle-widget/css/temp.css"
 
-# Merge all css files to 1 file
-$PROJECT_ROOT/scripts/merge-css.sh $PROJECT_ROOT/ui-bundle-docs/css/
-$PROJECT_ROOT/scripts/merge-css.sh $PROJECT_ROOT/ui-bundle-widget/css/
+# Merge all css files to 1 file excluding site.css
+find $PROJECT_ROOT/ui-bundle-docs/css/ -type f -not -name 'site.css' -name '*.css' -exec cat {} \; | npx postcss --use postcss-import postcss-clean --no-map > "$TEMP_FILE_DOCS"
+find $PROJECT_ROOT/ui-bundle-widget/css/ -type f -not -name 'site.css' -name '*.css' -exec cat {} \; | npx postcss --use postcss-import postcss-clean --no-map > "$TEMP_FILE_WIDGET"
 
-MERGE_FILE_DOCS="$PROJECT_ROOT/ui-bundle-docs/css/merged.css"
-MERGE_FILE_WIDGET="$PROJECT_ROOT/ui-bundle-widget/css/merged.css"
+# Append the header comment
+echo -e "/*! DO NOT EDIT: 'site.css' is auto-generated from the minified output of 'default-styles.css' and 'custom-styles.css'. */\n$(cat $TEMP_FILE_DOCS)" > "$TEMP_FILE_DOCS"
+echo -e "/*! DO NOT EDIT: 'site.css' is auto-generated from the minified output of 'default-styles.css' and 'custom-styles.css'. */\n$(cat $TEMP_FILE_WIDGET)" > "$TEMP_FILE_WIDGET"
 
-# Generate the minified CSS with custom header
-{
-  echo "/*! DO NOT EDIT: 'site.css' is auto-generated from the minified output of 'default-styles.css' and 'custom-styles.css'. */"
-  npx postcss "$MERGE_FILE_DOCS" --use postcss-import postcss-clean --no-map
-} > "$PROJECT_ROOT/ui-bundle-docs/css/site.css"
+# Calculate the original checksum
+ORIGINAL_CHECKSUM_DOCS=$(openssl dgst -md5 "$PROJECT_ROOT/ui-bundle-docs/css/site.css" | cut -d ' ' -f2)
+ORIGINAL_CHECKSUM_WIDGET=$(openssl dgst -md5 "$PROJECT_ROOT/ui-bundle-widget/css/site.css" | cut -d ' ' -f2)
 
-{
-  echo "/*! DO NOT EDIT: 'site.css' is auto-generated from the minified output of 'default-styles.css' and 'custom-styles.css'. */"
-  npx postcss "$MERGE_FILE_WIDGET" --use postcss-import postcss-clean --no-map
-} > "$PROJECT_ROOT/ui-bundle-widget/css/site.css"
+# Calculate the new checksum
+NEW_CHECKSUM_DOCS=$(openssl dgst -md5 "$TEMP_FILE_DOCS" | cut -d ' ' -f2)
+NEW_CHECKSUM_WIDGET=$(openssl dgst -md5 "$TEMP_FILE_WIDGET" | cut -d ' ' -f2)
 
+# Compare original and new checksum
+if [[ "$ORIGINAL_CHECKSUM_DOCS" == "$NEW_CHECKSUM_DOCS" ]]; then
+  rm "$TEMP_FILE_DOCS"
+else
+  mv "$TEMP_FILE_DOCS" "$PROJECT_ROOT/ui-bundle-docs/css/site.css"
+fi
 
-# Remove the temporary file
-rm "$MERGE_FILE_DOCS"
-rm "$MERGE_FILE_WIDGET"
+if [[ "$ORIGINAL_CHECKSUM_WIDGET" == "$NEW_CHECKSUM_WIDGET" ]]; then
+  rm "$TEMP_FILE_WIDGET"
+else
+  mv "$TEMP_FILE_WIDGET" "$PROJECT_ROOT/ui-bundle-widget/css/site.css"
+fi
