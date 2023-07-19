4f3d8077db2d7a2d200d20cb0fe3819c37374b33
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-07-19 16:52:01 -0700

    Changing output to table for readability

--- scripts/beautify-logs.py
+++ scripts/beautify-logs.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python3
+# Beautifies logs in 'logs/' and adds 'xref:' attributes so
+# maintainers can directly open files from the preview panel.
 
-# import necessary Python modules
 import os
 import re
 from collections import defaultdict
 
-
 # function to extract error information from a given log file
 def get_logs(logfile):
     # define the regex pattern to match error messages
-    error_regex = r"\[(.*?)\] ERROR \(asciidoctor\): target of (.*?) not found: (.*?)\n\s*file: (.*?)\n"
+    error_regex = r"\[(.*?)\] ERROR \(asciidoctor\): (target of image not found): (.*?)\n\s*file: (.*?)\n"
     # create a defaultdict to store the errors, categorized by error type and target
     errors = defaultdict(lambda: defaultdict(list))
 
@@ -19,7 +18,7 @@ def get_logs(logfile):
 
     # find matches of the regex pattern in the log content
     matches = re.findall(error_regex, log_content)
-    # for each match, store the timestamp, error type, target and file in the 'errors' dictionary
+    # for each match, store the timestamp, error type, error target and file in the 'errors' dictionary
     for match in matches:
         timestamp, error_type, error_target, file = match
         errors[error_type][error_target].append(file)
@@ -27,47 +26,35 @@ def get_logs(logfile):
     # return the dictionary of errors
     return errors
 
-
-# function to transform a file path into a different format for better referencing
-def transform_filepath(filepath):
-    # split the file path into parts
-    path_parts = filepath.split('/')
-
-    # find the index of 'docs' in the path parts
-    doc_index = path_parts.index('docs')
-
-    # create a new path starting from the part after 'docs'
-    new_path_parts = path_parts[doc_index + 1:]
-
-    # join the new path parts into a string to be used as link text, skipping the first two parts
-    new_file_path = "/".join(new_path_parts[2:])
-
-    # join the new path parts into a string to be used as the link
-    full_path = "/".join(new_path_parts)
-
-    # return the cross-reference link in AsciiDoc format
-    return f"xref:../{full_path}[{new_file_path}]"
-
-
 # function to write the error information back to the log file in a new format
 def reformat_errors(logfile, errors):
     # get the log type from the file name
     log_type = os.path.basename(logfile).split('-')[0].capitalize()
 
-    # open the log file for writing
+    # start the asciidoctor table
+    output = f'= {log_type} logs\n\n[cols="1,1,1,1"]\n|===\n|Type|Error|Module|File\n\n'
+
+    # for each error, write the type and target of the error, and the transformed file path
+    for error_type, targets in errors.items():
+        for target, files in targets.items():
+            for file in files:
+                # split the file path into parts
+                path_parts = file.split('/')
+                # find the index of 'modules' in the path parts
+                module_index = path_parts.index('modules')
+                # take the next part after 'modules' as the module name
+                module = path_parts[module_index + 1]
+                # create a new path starting from the part after 'docs'
+                new_path_parts = path_parts[path_parts.index('docs') + 1:]
+                # join the new path parts into a string
+                new_file_path = "/".join(new_path_parts)
+                output += f'|{error_type}\n|{target}\n|{module}\n|xref:../{new_file_path}[{new_path_parts[-1]}]\n'
+
+    output += '|==='
+
+    # open the log file for writing and write the output
     with open(logfile, 'w') as f:
-        # write the log type at the top of the file
-        f.write(f'= {log_type} logs\n')
-
-        # for each error, write the type and target of the error, and the transformed file path
-        for error_type, targets in errors.items():
-            f.write(f'\n== {error_type} not found\n')
-            for target, files in targets.items():
-                f.write(f'\n=== {target}\n\n')
-                for file in files:
-                    new_file_path = transform_filepath(file)
-                    f.write(f'- {new_file_path}\n')
-
+        f.write(output)
 
 # list of log files to process
 log_files = ['logs/docs-logs.adoc', 'logs/widget-logs.adoc']
