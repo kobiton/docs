0f60a01ca38dc70baea34c0ddb47750420048fd3
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-07-25 10:51:38 -0700

    Cleanup file

--- scripts/beautify-logs.py
+++ scripts/beautify-logs.py
@@ -10,18 +10,17 @@ from collections import defaultdict
 # Constants
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 LOG_FILES = [os.path.join(SCRIPT_DIR, '../logs/docs-logs.adoc'), os.path.join(SCRIPT_DIR, '../logs/widget-logs.adoc')]
+ANTORA_LEVELS = ['fatal', 'error', 'warn', 'info', 'debug']
 
 
 # Get the logs and create a dictionary for building beautified logs
 def get_logs(log):
     logs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-    log_types = set()
 
     with open(log, 'r') as f:
         for line in f:
             data = json.loads(line)
-            log_type = f"{data['level'].upper()} (asciidoctor)"
-            log_types.add(log_type)
+            log_type = data['level']
             absolute_file_path = data['file']['path']
             worktree_path = data['source']['worktree']
             issue, log_target = data['msg'].split(': ')
@@ -31,7 +30,7 @@ def get_logs(log):
 
             logs[log_type][issue][log_target].append(relative_file_path)
 
-    return logs, list(log_types)
+    return logs
 
 
 # Sort logs alphabetically by column: type, issue, module, file.
@@ -70,45 +69,44 @@ def count_issues(logs):
 
 # Reformat logs into a table
 def reformat_logs(logfile, logs):
-    antora_playbook = os.path.basename(logfile).split('-')[0].capitalize()
-    log_types = ['FATAL (asciidoctor)', 'ERROR (asciidoctor)', 'WARN (asciidoctor)', 'INFO (asciidoctor)',
-                 'DEBUG (asciidoctor)']
-
     # Get total issue counts
     total = count_issues(logs)
 
-    output = f'= {antora_playbook} logs\n\n' \
-             f'.Table of contents\n'
+    # Create file heading
+    antora_playbook = os.path.basename(logfile).split('-')[0].capitalize()
+    output = f'= {antora_playbook} logs\n\n'
 
     # Create a table of contents
-    for type in log_types:
+    output += f'.Table of contents\n'
+    for type in ANTORA_LEVELS:
         count = total.get(type, 0)
         count_str = f': {count} issue(s)' if count > 0 else ': _NONE_'
         output += f'* xref:_{type.split()[0].lower()}[]{count_str}\n'
 
     output += f'\n'
 
-    for type in log_types:
+    for type in ANTORA_LEVELS:
         # Add anchor to headers
         output += f'[#_{type.split()[0].lower()}]\n'
 
         # If there are logs for this log type, create a table
         if type in logs:
-            output += f'== {type.split()[0].capitalize()}\n\n' \
+            output += f'== {type.split()[0].upper()}\n\n' \
                       f'[cols="1,1,1,1"]\n' \
                       f'|===\n' \
                       f'|Issue type|Issue|Module|File\n\n'
             for issue, log_targets in logs[type].items():
                 for log_target, files in log_targets.items():
                     for file in files:
-                        # Extracting module and file name from relative path
+                        # Get the file cross-reference link from 'relative_file_path'
                         module = file.split('modules/')[1].split('/')[0]
                         file_name = file.split('/')[-1]
                         output += f'|{issue}\n|{log_target}\n|{module}\n|xref:..{file}[{file_name}]\n'
             output += '|===\n\n'
+        # If there are no logs for this log type, print NONE
         else:
             # If there are no logs, write NONE
-            output += f'== {type.split()[0].capitalize()}\n\n_NONE_\n\n'
+            output += f'== {type.split()[0].upper()}\n\n_NONE_\n\n'
 
     # Overwrite log file
     with open(logfile, 'w') as f:
@@ -117,10 +115,10 @@ def reformat_logs(logfile, logs):
 
 # Run script
 def run_script():
-    for logfile in LOG_FILES:
-        logs, log_types = get_logs(logfile)
+    for file in LOG_FILES:
+        logs = get_logs(file)
         sorted_logs = sort_logs(logs)
-        reformat_logs(logfile, sorted_logs)
+        reformat_logs(file, sorted_logs)
 
 
 run_script()
